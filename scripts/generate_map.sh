2e7e2499158c7854e5a02d44c979975c924a1844
Qiang Lu <buaaluqiang@hotmail.com>
2019-03-21 15:53:20 -0700

    Add generate_map.sh and add cmd to generate base_map.bin

---
+++ scripts/generate_map.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${DIR}/.."
+
+source "$DIR/apollo_base.sh"
+
+if [ $# -eq 0 ]; then
+  echo "Please specify name of your map directory."
+else
+  dir_name=modules/map/data/$1
+  bazel-bin/modules/map/tools/sim_map_generator --map_dir=${dir_name} --output_dir=${dir_name}
+  bash scripts/generate_routing_topo_graph.sh --map_dir ${dir_name}
+  protoc -I /apollo --encode=apollo.hdmap.Map /apollo/modules/map/proto/map.proto < ${dir_name}/base_map.txt > ${dir_name}/base_map.bin
+fi
+
