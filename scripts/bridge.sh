81e704ecb8a2a40141ea4abbbc3a6eba2ccd484d
Qiang Lu <buaaluqiang@hotmail.com>
2019-03-29 17:29:32 -0700

    Move decompress from bridge.sh to bootstrap.sh

--- scripts/bridge.sh
+++ scripts/bridge.sh
@@ -3,8 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${DIR}/.."
 
-source "$DIR/apollo_base.sh"
-
-echo `pwd`
 bazel-bin/cyber/bridge/cyber_bridge
-mainboard -d modules/drivers/tools/image_decompress/dag/image_decompress.dag  
