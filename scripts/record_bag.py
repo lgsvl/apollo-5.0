ApolloAuto/apollo

record_bag.py: sort LARGE_TOPICS in alphabetical order

scripts/record_bag.py
@@ -47,33 +47,33 @@ import psutil
 LARGE_TOPICS = [
     '/apollo/sensor/camera/front_12mm/image',
     '/apollo/sensor/camera/front_12mm/image/compressed',
+    '/apollo/sensor/camera/front_12mm/video/compressed',
     '/apollo/sensor/camera/front_6mm/image',
     '/apollo/sensor/camera/front_6mm/image/compressed',
+    '/apollo/sensor/camera/front_6mm/video/compressed',
     '/apollo/sensor/camera/left_fisheye/image',
     '/apollo/sensor/camera/left_fisheye/image/compressed',
+    '/apollo/sensor/camera/left_fisheye/video/compressed',
     '/apollo/sensor/camera/rear_6mm/image',
     '/apollo/sensor/camera/rear_6mm/image/compressed',
+    '/apollo/sensor/camera/rear_6mm/video/compressed',
     '/apollo/sensor/camera/right_fisheye/image',
     '/apollo/sensor/camera/right_fisheye/image/compressed',
-    '/apollo/sensor/camera/front_12mm/video/compressed',
-    '/apollo/sensor/camera/front_6mm/video/compressed',
-    '/apollo/sensor/camera/left_fisheye/video/compressed',
-    '/apollo/sensor/camera/rear_6mm/video/compressed',
     '/apollo/sensor/camera/right_fisheye/video/compressed',
-    '/apollo/sensor/radar/front',
-    '/apollo/sensor/radar/rear',
-    '/apollo/sensor/lidar16/front/center/PointCloud2',
-    '/apollo/sensor/lidar16/front/up/PointCloud2',
-    '/apollo/sensor/lidar16/rear/left/PointCloud2',
-    '/apollo/sensor/lidar16/rear/right/PointCloud2',
-    '/apollo/sensor/lidar16/fusion/PointCloud2',
-    '/apollo/sensor/lidar16/fusion/compensator/PointCloud2',
     '/apollo/sensor/lidar128/PointCloud2',
     '/apollo/sensor/lidar128/compensator/PointCloud2',
-    '/apollo/sensor/lidar16/Scan',
     '/apollo/sensor/lidar16/PointCloud2',
+    '/apollo/sensor/lidar16/Scan',
     '/apollo/sensor/lidar16/compensator/PointCloud2',
+    '/apollo/sensor/lidar16/front/center/PointCloud2',
+    '/apollo/sensor/lidar16/front/up/PointCloud2',
+    '/apollo/sensor/lidar16/fusion/PointCloud2',
+    '/apollo/sensor/lidar16/fusion/compensator/PointCloud2',
+    '/apollo/sensor/lidar16/rear/left/PointCloud2',
+    '/apollo/sensor/lidar16/rear/right/PointCloud2',
     '/apollo/sensor/microphone',
+    '/apollo/sensor/radar/front',
+    '/apollo/sensor/radar/rear',
     '/apollo/sensor/velodyne64/compensator/PointCloud2',
 ]
 

lgsvl/apollo-5.0

scripts/record_bag.py
@@ -47,30 +47,30 @@ import psutil
 LARGE_TOPICS = [
     '/apollo/sensor/camera/front_12mm/image',
     '/apollo/sensor/camera/front_12mm/image/compressed',
+    '/apollo/sensor/camera/front_12mm/video/compressed',
     '/apollo/sensor/camera/front_6mm/image',
     '/apollo/sensor/camera/front_6mm/image/compressed',
+    '/apollo/sensor/camera/front_6mm/video/compressed',
     '/apollo/sensor/camera/left_fisheye/image',
     '/apollo/sensor/camera/left_fisheye/image/compressed',
+    '/apollo/sensor/camera/left_fisheye/video/compressed',
     '/apollo/sensor/camera/rear_6mm/image',
     '/apollo/sensor/camera/rear_6mm/image/compressed',
+    '/apollo/sensor/camera/rear_6mm/video/compressed',
     '/apollo/sensor/camera/right_fisheye/image',
     '/apollo/sensor/camera/right_fisheye/image/compressed',
-    '/apollo/sensor/camera/front_12mm/video/compressed',
-    '/apollo/sensor/camera/front_6mm/video/compressed',
-    '/apollo/sensor/camera/left_fisheye/video/compressed',
-    '/apollo/sensor/camera/rear_6mm/video/compressed',
     '/apollo/sensor/camera/right_fisheye/video/compressed',
-    '/apollo/sensor/radar/front',
-    '/apollo/sensor/radar/rear',
+    '/apollo/sensor/lidar128/compensator/PointCloud2',
+    '/apollo/sensor/lidar128/PointCloud2',
+    '/apollo/sensor/lidar16/compensator/PointCloud2',
     '/apollo/sensor/lidar16/front/center/PointCloud2',
     '/apollo/sensor/lidar16/front/up/PointCloud2',
+    '/apollo/sensor/lidar16/fusion/compensator/PointCloud2',
+    '/apollo/sensor/lidar16/fusion/PointCloud2',
+    '/apollo/sensor/lidar16/PointCloud2',
     '/apollo/sensor/lidar16/rear/left/PointCloud2',
     '/apollo/sensor/lidar16/rear/right/PointCloud2',
-    '/apollo/sensor/lidar16/fusion/PointCloud2',
-    '/apollo/sensor/lidar16/fusion/compensator/PointCloud2',
     '/apollo/sensor/velodyne64/compensator/PointCloud2',
-    '/apollo/sensor/lidar128/PointCloud2',
-    '/apollo/sensor/lidar128/compensator/PointCloud2',
 ]
 
 